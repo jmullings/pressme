ecbdb7824d8141c48a15050c44a403ca32c04f9d
BlackCellReact <jason.mullings@calatacorp.com>
2018-06-21 15:21:08 +0800

    add lock file

--- web/index.php
+++ web/index.php
@@ -1,16 +1,17 @@
 <?php
 
 require('../vendor/autoload.php');
-
-$loop = React\EventLoop\Factory::create();
-$stream = new React\Stream\ReadableResourceStream(fopen('access.log.gz', 'r'), $loop);
-
-$decompressor = ZlibFilterStream::createGzipDecompressor();
-
-$decompressor->on('data', function ($data) {
-    echo $data;
-});
-
-$stream->pipe($decompressor);
-
-$loop->run();
+//
+//$loop = React\EventLoop\Factory::create();
+//$stream = new React\Stream\ReadableResourceStream(fopen('access.log.gz', 'r'), $loop);
+//
+//$decompressor = ZlibFilterStream::createGzipDecompressor();
+//
+//$decompressor->on('data', function ($data) {
+//    echo $data;
+//});
+//
+//$stream->pipe($decompressor);
+//
+//$loop->run();
+echo 'This is a test';
